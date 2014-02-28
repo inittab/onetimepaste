e9c70387c3a61cb87b9a87d53b17921c85cb626c
Alberto Gonzalez Iniesta <agi@inittab.org>
2014-02-28 16:34:06 +0100

    Improve "Message not found" error display

--- templates/recover_fail.php
+++ templates/recover_fail.php
@@ -1,2 +1,2 @@
-<p class="error">El mensaje no se encuentra en el sistema.</p>
-<p class="normal">El mensaje ya ha sido leido, ha caducado o la dirección del mensaje es errónea.</p>
+<p class="normal">El mensaje no se encuentra en el sistema.</p>
+<p class="error" align="center">ATENCIÓN: El mensaje ya ha sido leido, ha caducado o la dirección del mensaje es errónea.</p>
