f2741747c0cf4f51d55dfc7deb2077ad51c9ad77
Alberto Gonzalez Iniesta <agi@inittab.org>
2014-03-04 14:10:12 +0100

    Rework mysql backend by sils@sils.net

--- storage/mysql.php
+++ storage/mysql.php
@@ -6,64 +6,66 @@ include_once "$backend.config.php";
 
 function db_connect() {
 	global $mysql_host, $mysql_db, $mysql_user, $mysql_pass;
-	return new PDO("mysql:host=$mysql_host;dbname=$mysql_db", $mysql_user, $mysql_pass, array(PDO::ATTR_PERSISTENT => true));
+	return new PDO("mysql:host=$mysql_host;dbname=$mysql_db", $mysql_user, $mysql_pass, array(PDO::ATTR_PERSISTENT => true, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 
-function read_msg($id) {
-	$dbh = db_connect();
-	$sth = $dbh->prepare('SELECT id, message, time FROM pastes
-                      WHERE id=:id');
-	$sth->bindParam(':id', $id, PDO::PARAM_STR);
-	$sth->execute();
-
-	if($sth->rowCount() == 1) {
-		$result = $sth->fetch(PDO::FETCH_ASSOC);
-		$message = $result["message"];
-		#$time=$result["time"];
+function delete_msg($id='') {
+	try {
+		$dbh = db_connect();
 		$sth = $dbh->prepare('DELETE FROM pastes WHERE id=:id');
 		$sth->bindParam(':id', $id, PDO::PARAM_STR);
 		$sth->execute();
-		$error=$sth->errorInfo();
-		if($error[0] != "00000") {
-			## TODO error handling
-			print "ewwwww";
-			return False;
-		}
-		return $message;
-	} else {
-		return False;
+	} catch (PDOException $e) {
+		#print  "<div class=\"error\">Message deletion failed</div>";
+		return false;
 	}
+	return True;
 }
 
-function save_msg($message,$id) {
-	$dbh = db_connect();
-	$sth = $dbh->prepare('INSERT INTO pastes (id, message, time) 
-                    VALUES (:id, :message, NOW())');
-	$sth->bindParam(':id', $id, PDO::PARAM_STR);
-	$sth->bindParam(':message', $message, PDO::PARAM_STR);
-	# insert it!
-	$sth->execute();
+function save_msg($message='',$id='') {
+	try {
+		$dbh = db_connect();
+		$sth = $dbh->prepare('INSERT INTO pastes (id, message, time) VALUES (:id, :message, NOW())');
+		$sth->bindParam(':id', $id, PDO::PARAM_STR);
+		$sth->bindParam(':message', $message, PDO::PARAM_STR);
+		$sth->execute();
+	} catch (PDOException $e) {
+		#print  "<div class=\"error\">Message storage failed</div>";
+		return false;
+	}
+	return True;
+}
 
-	## TODO error handling
-	$error=$sth->errorInfo();
-	if($error[0] == "00000") {
-		return True;
-	} else {
-		return False;
+function read_msg($id='') {
+	try {
+		$dbh = db_connect();
+		$sth = $dbh->prepare('SELECT id, message, time FROM pastes WHERE id=:id');
+		$sth->bindParam(':id', $id, PDO::PARAM_STR);
+		$sth->execute();
+
+		if($sth->rowCount() == 1) {
+			$result = $sth->fetch(PDO::FETCH_ASSOC);
+			delete_msg($id);
+			return $result["message"];
+		} else{
+			return false;
+		}
+	} catch (PDOException $e) {
+		#print  "<div class=\"error\">Message storage failed</div>";
+		return false;
 	}
 }
 
 function purge_old() {
 	global $hours_expire;
-	$dbh = db_connect();
-	$sth = $dbh->prepare('DELETE FROM pastes WHERE time < (NOW() - INTERVAL :hours HOUR)');
-	$sth->bindParam(':hours', $hours_expire, PDO::PARAM_INT);
-	$sth->execute();
-	$error=$sth->errorInfo();
-	if($error[0] != "00000") {
-		## TODO error handling
-		print "ewwwww";
-		return False;
+	try {
+		$dbh = db_connect();
+		$sth = $dbh->prepare('DELETE FROM pastes WHERE time < (NOW() - INTERVAL :hours HOUR)');
+		$sth->bindParam(':hours', $hours_expire, PDO::PARAM_INT);
+		$sth->execute();
+	} catch (PDOException $e) {
+		print  "<div class=\"error\">Message deletion failed</div>";
+		return false;
 	}
 	return True;
 }
