85ba67fc5d1ddbd5b53d4c2be9452a395ecb76e3
Alberto Gonzalez Iniesta <agi@inittab.org>
2014-03-04 18:48:17 +0100

    Auto-detect $base_url

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 # Site URL, you should REALLY run it under https
-$base_url="https://CHANGE_THIS_TO_YOUR_SITE_ADDRESS";
+$base_url="https://".$_SERVER['HTTP_HOST'].pathinfo($_SERVER['PHP_SELF'])['dirname'];
+# you may want to override the auto-detected URL
+#$base_url="https://CHANGE_THIS_TO_YOUR_SITE_ADDRESS";
+
 # storage backend, storage config in storage/_BACKEND_.config.php
 $backend="textfiles";
 #$backend="mysql";
+
 # days till an unread message is purged from the DB
 # remember to set a cronjob like
 # 0 * * * * YOUR_USER /usr/bin/curl https://1tp.es/cron.php
