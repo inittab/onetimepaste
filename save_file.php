75d21a2841a196e4643f928ae4045ae15b8bea5e
Alberto Gonzalez Iniesta <agi@inittab.org>
2014-03-26 21:30:47 +0100

    Only generate id & key after file is validated

--- save_file.php
+++ save_file.php
@@ -4,15 +4,14 @@ if(!defined('INCLUDED_FROM_INDEX')){ die(); }
 include_once "random.php";
 include_once "storage/$backend.php";
 
-$id=random_text('alnum', 20);
-$key=random_text('alnum', 10);
-
 # Check $_FILES array
 if(isset($_FILES) && sizeof($_FILES) > 0) {
 	if($max_upload_size > 0 && $_FILES['file']['size'] > $max_upload_size * 1024 * 1024) {
 		include "templates/file_too_large.php";
 	}
 	elseif(isset($_FILES['file']['error']) && $_FILES['file']['error'] == 0) {
+		$id=random_text('alnum', 20);
+		$key=random_text('alnum', 10);
 		include "encryption.php";
 		$filename = encrypt_message($_FILES['file']['name'], $key);
 		$filecontents = encrypt_message(file_get_contents($_FILES['file']['tmp_name']), $key);
