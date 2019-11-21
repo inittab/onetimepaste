3717e46df51bcf42ae4ca71b76aa2d2c064b52d3
Alberto Gonzalez Iniesta <agi@inittab.org>
2019-11-21 11:40:56 +0100

    encryption.php: move from mcrypt to openssl

--- encryption.php
+++ encryption.php
@@ -1,40 +1,12 @@
 <?php
-# Open crypt module
-function open_crypt_module($key){
-	$algorithm=MCRYPT_RIJNDAEL_256;
-	$algorithm_mode=MCRYPT_MODE_CBC;
-	$cipher=mcrypt_module_open($algorithm, '', $algorithm_mode, '');
-	# $iv has to be the same on encryption and decryption
-	# store it with the message or use a fixed one
-	#$iv_size = mcrypt_enc_get_iv_size($cipher);
-	#$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-	$iv="this is an init vector..........";
-
-	if(!mcrypt_generic_init($cipher,$key,$iv) != -1) {
-		close_crypt_module();
-		exit(0);
-	}
-	return $cipher;
-}
-
-# Close crypt module
-function close_crypt_module() {
-	global $cipher;
-
-	if (isset($cipher)) {
-		mcrypt_generic_deinit($cipher);
-		mcrypt_module_close($cipher);
-	}
-}
-
 # Encrypt message
 function encrypt_message($message,$key) {
 	(string) $encrypted = '';
 
 	if(isset($message) && isset($key)) {
-		$cipher=open_crypt_module($key);
-		$encrypted=bin2hex(mcrypt_generic($cipher,$message));
-		close_crypt_module();
+		$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+		$encrypted_msg = openssl_encrypt($message, 'aes-256-cbc', $key, 0, $iv);
+		$encrypted=base64_encode($encrypted_msg . '::' . $iv);
 	}
 	return $encrypted;
 }
@@ -44,9 +16,8 @@ function decrypt_message($message,$key) {
 	(string) $decrypted = '';
 
 	if(isset($message) && isset($key)) {
-		$cipher=open_crypt_module($key);
-		$decrypted=mdecrypt_generic($cipher,hex2bin($message));
-		close_crypt_module();
+		list($encrypted_data, $iv) = explode('::', base64_decode($message), 2);
+		$decrypted=openssl_decrypt($encrypted_data, 'aes-256-cbc', $key, 0, $iv);
 	}
 	return $decrypted;
 }
